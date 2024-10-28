b0968974f2144b62deef3a8e987a2c6a03f26a7a
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2024-10-28 00:53:51 -0300

    Added support to detect the transition between lower and upper case characters.

--- variable-name-case-converter.js
+++ variable-name-case-converter.js
@@ -3,14 +3,18 @@ function removeAccents(str) {
 
 function toKebabCase(str) {
     return removeAccents(str)
+        .replace(/([a-z])([A-Z])/g, '$1-$2')
         .toLowerCase()
         .replace(/[_/]/g, '-')
         .replace(/[^a-z0-9-]+/g, '-')
         .replace(/-+/g, '-')
         .replace(/^-|-$/g, '');}
 
-const input = process.argv[2];
-if (input)
-    console.log(toKebabCase(input));
-else
-    console.log("Please provide a string as an argument.");
+function main() {
+    const input = process.argv[2];
+    if (input)
+        console.log(toKebabCase(input));
+    else
+        console.log("Please provide a string as an argument.");}
+
+main();
