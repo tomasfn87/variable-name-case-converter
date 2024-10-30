0c3116340678a284cfbd755c7de7ac7b73dacd54
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2024-10-29 23:00:27 -0300

    Refactored unit tests' input values.

--- test/variable-name-case-converter.test.js
+++ test/variable-name-case-converter.test.js
@@ -3,296 +3,335 @@ import { runTestArray } from './run-test-array.js';
 import { listWords, toCamelCase, toKebabCase, toPascalCase, toSnakeCase }
     from '../variable-name-case-converter.js';
 
-const expected = {
-    "list": [ "stop", "being", "a", "drug", "addict" ],
-    "camel": "stopBeingADrugAddict",
-    "kebab": "stop-being-a-drug-addict",
-    "pascal": "StopBeingADrugAddict",
-    "pascalSnake": "Stop_Being_A_Drug_Addict",
-    "screamingSnake": "STOP_BEING_A_DRUG_ADDICT",
-    "snake": "stop_being_a_drug_addict"};
+const inputs = {
+    camel: "stopBeingADrugAddict",
+    kebab: "stop-being-a-drug-addict",
+    natural: "Stop being a drug addict!",
+    pascal: "StopBeingADrugAddict",
+    pascalSnake: "Stop_Being_A_Drug_Addict",
+    screamingSnake: "STOP_BEING_A_DRUG_ADDICT",
+    snake: "stop_being_a_drug_addict",
+    spacedLower: "stop being a drug addict",
+    spacedUpper: "STOP BEING A DRUG ADDICT"};
 
-[
-    {
-        title:   'List Words (listWords)',
-        fn:      listWords,
-        testArr: [
-            {
-                value: "StopBeingADrugAddict",
-                result: expected.list
-            },
-            {
-                value: "stop being a drug addict",
-                result: expected.list
-            },
-            {
-                value: "stop-being-a-drug-addict",
-                result: expected.list
-            },
-            {
-                value: "stop_being_a_drug_addict",
-                result: expected.list
-            },
-            {
-                value: "Stop_Being_A_Drug_Addict",
-                result: expected.list
-            },
-            {
-                value: "STOP BEING A DRUG ADDICT",
-                result: expected.list
-            },
-            {
-                value: "STOP_BEING_A_DRUG_ADDICT",
-                result: expected.list
-            },
-            {
-                value: "Stop being a drug addict!",
-                result: expected.list
-            }
-        ]
-    },
-    {
-        title:   'To Camel Case (toCamelCase)',
-        fn:      toCamelCase,
-        testArr: [
-            {
-                value: "StopBeingADrugAddict",
-                result: expected.camel
-            },
-            {
-                value: "stop being a drug addict",
-                result: expected.camel
-            },
-            {
-                value: "stop-being-a-drug-addict",
-                result: expected.camel
-            },
-            {
-                value: "stop_being_a_drug_addict",
-                result: expected.camel
-            },
-            {
-                value: "Stop_Being_A_Drug_Addict",
-                result: expected.camel
-            },
-            {
-                value: "STOP BEING A DRUG ADDICT",
-                result: expected.camel
-            },
-            {
-                value: "STOP_BEING_A_DRUG_ADDICT",
-                result: expected.camel
-            },
-            {
-                value: "Stop being a drug addict!",
-                result: expected.camel
-            }
-        ]
-    },
-    {
-        title:   'To Kebab Case (toKebabCase)',
-        fn:      toKebabCase,
-        testArr: [
-            {
-                value: "StopBeingADrugAddict",
-                result: expected.kebab
-            },
-            {
-                value: "stop being a drug addict",
-                result: expected.kebab
-            },
-            {
-                value: "stop-being-a-drug-addict",
-                result: expected.kebab
-            },
-            {
-                value: "stop_being_a_drug_addict",
-                result: expected.kebab
-            },
-            {
-                value: "Stop_Being_A_Drug_Addict",
-                result: expected.kebab
-            },
-            {
-                value: "STOP BEING A DRUG ADDICT",
-                result: expected.kebab
-            },
-            {
-                value: "STOP_BEING_A_DRUG_ADDICT",
-                result: expected.kebab
-            },
-            {
-                value: "Stop being a drug addict!",
-                result: expected.kebab
-            }
-        ]
-    },
-    {
-        title:   'To Pascal Case (toPascalCase)',
-        fn:      toPascalCase,
-        testArr: [
-            {
-                value: "StopBeingADrugAddict",
-                result: expected.pascal
-            },
-            {
-                value: "stop being a drug addict",
-                result: expected.pascal
-            },
-            {
-                value: "stop-being-a-drug-addict",
-                result: expected.pascal
-            },
-            {
-                value: "stop_being_a_drug_addict",
-                result: expected.pascal
-            },
-            {
-                value: "Stop_Being_A_Drug_Addict",
-                result: expected.pascal
-            },
-            {
-                value: "STOP BEING A DRUG ADDICT",
-                result: expected.pascal
-            },
-            {
-                value: "STOP_BEING_A_DRUG_ADDICT",
-                result: expected.pascal
-            },
-            {
-                value: "Stop being a drug addict!",
-                result: expected.pascal
-            }
-        ]
-    },
-    {
-        title:   'To Pascal Case (toPascalCase); mode="snake"',
-        fn:      toPascalCase,
-        testArr: [
-            {
-                value: "StopBeingADrugAddict",
-                args:  ["snake"],
-                result: expected.pascalSnake
-            },
-            {
-                value: "stop being a drug addict",
-                args:  ["snake"],
-                result: expected.pascalSnake
-            },
-            {
-                value: "stop-being-a-drug-addict",
-                args:  ["snake"],
-                result: expected.pascalSnake
-            },
-            {
-                value: "stop_being_a_drug_addict",
-                args:  ["snake"],
-                result: expected.pascalSnake
-            },
-            {
-                value: "Stop_Being_A_Drug_Addict",
-                args:  ["snake"],
-                result: expected.pascalSnake
-            },
-            {
-                value: "STOP BEING A DRUG ADDICT",
-                args:  ["snake"],
-                result: expected.pascalSnake
-            },
-            {
-                value: "STOP_BEING_A_DRUG_ADDICT",
-                args:  ["snake"],
-                result: expected.pascalSnake
-            },
-            {
-                value: "Stop being a drug addict!",
-                args:  ["snake"],
-                result: expected.pascalSnake
-            }
-        ]
-    },
-    {
-        title:   'To Snake Case (toSnakeCase)',
-        fn:      toSnakeCase,
-        testArr: [
-            {
-                value: "StopBeingADrugAddict",
-                result: expected.snake
-            },
-            {
-                value: "stop being a drug addict",
-                result: expected.snake
-            },
-            {
-                value: "stop-being-a-drug-addict",
-                result: expected.snake
-            },
-            {
-                value: "stop_being_a_drug_addict",
-                result: expected.snake
-            },
-            {
-                value: "Stop_Being_A_Drug_Addict",
-                result: expected.snake
-            },
-            {
-                value: "STOP BEING A DRUG ADDICT",
-                result: expected.snake
-            },
-            {
-                value: "STOP_BEING_A_DRUG_ADDICT",
-                result: expected.snake
-            },
-            {
-                value: "Stop being a drug addict!",
-                result: expected.snake
-            }
-        ]
-    },
-    {
-        title:   'To Snake Case (toSnakeCase); mode="screaming"',
-        fn:      toSnakeCase,
-        testArr: [
-            {
-                value: "StopBeingADrugAddict",
-                args:  ["screaming"],
-                result: expected.screamingSnake
-            },
-            {
-                value: "stop being a drug addict",
-                args:  ["screaming"],
-                result: expected.screamingSnake
-            },
-            {
-                value: "stop-being-a-drug-addict",
-                args:  ["screaming"],
-                result: expected.screamingSnake
-            },
-            {
-                value: "stop_being_a_drug_addict",
-                args:  ["screaming"],
-                result: expected.screamingSnake
-            },
-            {
-                value: "Stop_Being_A_Drug_Addict",
-                args:  ["screaming"],
-                result: expected.screamingSnake
-            },
-            {
-                value: "STOP BEING A DRUG ADDICT",
-                args:  ["screaming"],
-                result: expected.screamingSnake
-            },
-            {
-                value: "STOP_BEING_A_DRUG_ADDICT",
-                args:  ["screaming"],
-                result: expected.screamingSnake
-            },
-            {
-                value: "Stop being a drug addict!",
-                args:  ["screaming"],
-                result: expected.screamingSnake
-            }
-        ]
-    }
-].forEach(e => runTestArray( e.title, e.fn, e.testArr ))
+const expected = {
+    list: [ "stop", "being", "a", "drug", "addict" ],
+    camel: "stopBeingADrugAddict",
+    kebab: "stop-being-a-drug-addict",
+    pascal: "StopBeingADrugAddict",
+    pascalSnake: "Stop_Being_A_Drug_Addict",
+    screamingSnake: "STOP_BEING_A_DRUG_ADDICT",
+    snake: "stop_being_a_drug_addict"};
+
+[{
+    title:   'List Words (listWords)',
+    fn:      listWords,
+    testArr: [
+        {
+            value: inputs.camel,
+            result: expected.list
+        },
+        {
+            value: inputs.pascal,
+            result: expected.list
+        },
+        {
+            value: inputs.kebab,
+            result: expected.list
+        },
+        {
+            value: inputs.pascalSnake,
+            result: expected.list
+        },
+        {
+            value: inputs.screamingSnake,
+            result: expected.list
+        },
+        {
+            value: inputs.snake,
+            result: expected.list
+        },
+        {
+            value: inputs.spacedLower,
+            result: expected.list
+        },
+        {
+            value: inputs.spacedUpper,
+            result: expected.list
+        },
+        {
+            value: inputs.natural,
+            result: expected.list
+        }
+    ]
+},
+{
+    title:   'To Camel Case (toCamelCase)',
+    fn:      toCamelCase,
+    testArr: [
+        {
+            value: inputs.camel,
+            result: expected.camel
+        },
+        {
+            value: inputs.pascal,
+            result: expected.camel
+        },
+        {
+            value: inputs.kebab,
+            result: expected.camel
+        },
+        {
+            value: inputs.pascalSnake,
+            result: expected.camel
+        },
+        {
+            value: inputs.screamingSnake,
+            result: expected.camel
+        },
+        {
+            value: inputs.snake,
+            result: expected.camel
+        },
+        {
+            value: inputs.spacedLower,
+            result: expected.camel
+        },
+        {
+            value: inputs.spacedUpper,
+            result: expected.camel
+        },
+        {
+            value: inputs.natural,
+            result: expected.camel
+        }
+    ]
+},
+{
+    title:   'To Kebab Case (toKebabCase)',
+    fn:      toKebabCase,
+    testArr: [
+        {
+            value: inputs.camel,
+            result: expected.kebab
+        },
+        {
+            value: inputs.pascal,
+            result: expected.kebab
+        },
+        {
+            value: inputs.kebab,
+            result: expected.kebab
+        },
+        {
+            value: inputs.pascalSnake,
+            result: expected.kebab
+        },
+        {
+            value: inputs.screamingSnake,
+            result: expected.kebab
+        },
+        {
+            value: inputs.snake,
+            result: expected.kebab
+        },
+        {
+            value: inputs.spacedLower,
+            result: expected.kebab
+        },
+        {
+            value: inputs.spacedUpper,
+            result: expected.kebab
+        },
+        {
+            value: inputs.natural,
+            result: expected.kebab
+        }
+    ]
+},
+{
+    title:   'To Pascal Case (toPascalCase)',
+    fn:      toPascalCase,
+    testArr: [
+        {
+            value: inputs.camel,
+            result: expected.pascal
+        },
+        {
+            value: inputs.pascal,
+            result: expected.pascal
+        },
+        {
+            value: inputs.kebab,
+            result: expected.pascal
+        },
+        {
+            value: inputs.pascalSnake,
+            result: expected.pascal
+        },
+        {
+            value: inputs.screamingSnake,
+            result: expected.pascal
+        },
+        {
+            value: inputs.snake,
+            result: expected.pascal
+        },
+        {
+            value: inputs.spacedLower,
+            result: expected.pascal
+        },
+        {
+            value: inputs.spacedUpper,
+            result: expected.pascal
+        },
+        {
+            value: inputs.natural,
+            result: expected.pascal
+        }
+    ]
+},
+{
+    title:   'To Pascal Case (toPascalCase); mode="snake"',
+    fn:      toPascalCase,
+    testArr: [
+        {
+            value: inputs.camel,
+            args:  ["snake"],
+            result: expected.pascalSnake
+        },
+        {
+            value: inputs.pascal,
+            args:  ["snake"],
+            result: expected.pascalSnake
+        },
+        {
+            value: inputs.kebab,
+            args:  ["snake"],
+            result: expected.pascalSnake
+        },
+        {
+            value: inputs.pascalSnake,
+            args:  ["snake"],
+            result: expected.pascalSnake
+        },
+        {
+            value: inputs.screamingSnake,
+            args:  ["snake"],
+            result: expected.pascalSnake
+        },
+        {
+            value: inputs.snake,
+            args:  ["snake"],
+            result: expected.pascalSnake
+        },
+        {
+            value: inputs.spacedLower,
+            args:  ["snake"],
+            result: expected.pascalSnake
+        },
+        {
+            value: inputs.spacedUpper,
+            args:  ["snake"],
+            result: expected.pascalSnake
+        },
+        {
+            value: inputs.natural,
+            args:  ["snake"],
+            result: expected.pascalSnake
+        }
+    ]
+},
+{
+    title:   'To Snake Case (toSnakeCase)',
+    fn:      toSnakeCase,
+    testArr: [
+        {
+            value: inputs.camel,
+            result: expected.snake
+        },
+        {
+            value: inputs.pascal,
+            result: expected.snake
+        },
+        {
+            value: inputs.kebab,
+            result: expected.snake
+        },
+        {
+            value: inputs.pascalSnake,
+            result: expected.snake
+        },
+        {
+            value: inputs.screamingSnake,
+            result: expected.snake
+        },
+        {
+            value: inputs.snake,
+            result: expected.snake
+        },
+        {
+            value: inputs.spacedLower,
+            result: expected.snake
+        },
+        {
+            value: inputs.spacedUpper,
+            result: expected.snake
+        },
+        {
+            value: inputs.natural,
+            result: expected.snake
+        }
+    ]
+},
+{
+    title:   'To Snake Case (toSnakeCase); mode="screaming"',
+    fn:      toSnakeCase,
+    testArr: [
+        {
+            value: inputs.camel,
+            args:  ["screaming"],
+            result: expected.screamingSnake
+        },
+        {
+            value: inputs.pascal,
+            args:  ["screaming"],
+            result: expected.screamingSnake
+        },
+        {
+            value: inputs.kebab,
+            args:  ["screaming"],
+            result: expected.screamingSnake
+        },
+        {
+            value: inputs.pascalSnake,
+            args:  ["screaming"],
+            result: expected.screamingSnake
+        },
+        {
+            value: inputs.screamingSnake,
+            args:  ["screaming"],
+            result: expected.screamingSnake
+        },
+        {
+            value: inputs.snake,
+            args:  ["screaming"],
+            result: expected.screamingSnake
+        },
+        {
+            value: inputs.spacedLower,
+            args:  ["screaming"],
+            result: expected.screamingSnake
+        },
+        {
+            value: inputs.spacedUpper,
+            args:  ["screaming"],
+            result: expected.screamingSnake
+        },
+        {
+            value: inputs.natural,
+            args:  ["screaming"],
+            result: expected.screamingSnake
+        }
+    ]
+}].forEach(e => runTestArray( e.title, e.fn, e.testArr ))
